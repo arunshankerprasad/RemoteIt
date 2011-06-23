f99b8144a9b220115e51ddf3ac0ee9648bc8aef9
Arun Shanker Prasad <arunshanker@qburst.com>
2011-06-23 13:56:54 +0530

    Code Commit

---
+++ src/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.remoteit.boolpref", false);
+pref("extensions.remoteit.intpref", 0);
+pref("extensions.remoteit.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.asprasad@asp.com.description", "chrome://remoteit/locale/overlay.properties");
